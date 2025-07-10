cb145ecd334ba3d2364a458f04a02dd7bde5c21c
abdulrahman-aiml <your.email@example.com>
2025-07-11 01:43:32 +0530

    update

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from src.medex.term_ex import MedicalTermsExtraction
-from src.medex.file_converter import texter
-import os 
+# from src.medex.term_ex import MedicalTermsExtraction
+# from src.medex.file_converter import texter
+# import os 
 
 
-def main():
-    path =r"C:\Users\admin dell\Desktop\RAG series\Naive RAG\src\resources\record2.txt"
-    api_key = os.getenv('GeminiApiKey')
-    model = "gemini-2.5-flash"
-    text = texter(path)
-    mte = MedicalTermsExtraction(api_key, model, text)
-    print("===== Diagnosis terms ==========")
-    mte.dextraction()
-    print("\n===== Procedural terms ==========")
-    mte.pextraction()
-    print("\n===== Modifier terms ==========")
-    mte.mextraction()
+# def main():
+#     path =r"C:\Users\admin dell\Desktop\RAG series\Naive RAG\src\resources\record2.txt"
+#     api_key = os.getenv('GeminiApiKey')
+#     model = "gemini-2.5-flash"
+#     text = texter(path)
+#     mte = MedicalTermsExtraction(api_key, model, text)
+#     print("===== Diagnosis terms ==========")
+#     mte.dextraction()
+#     print("\n===== Procedural terms ==========")
+#     mte.pextraction()
+#     print("\n===== Modifier terms ==========")
+#     mte.mextraction()
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
